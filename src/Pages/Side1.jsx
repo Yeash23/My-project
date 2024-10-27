1250abe37a8ae817d1b0d7995c9ab1d8438ac07d
mdalvinsifat <mdalvinsifat@gmail.com>
2024-10-28 01:40:49 +0600

    some

--- src/Pages/Side1.jsx
+++ src/Pages/Side1.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import image from '../assets/images/image.webp';
 import image1 from '../assets/images/image2.webp';
-import './Custome.css';
 import { Link } from 'react-router-dom';
 
 const Side1 = () => {
     return (
-        <div className="image-container mx-auto">
-            <img src={image} alt="First Image" className="image" />
-            <img src={image1} alt="Second Image" className="image" />
+        <div className="flex flex-col items-center bg-gradient-to-r from-blue-100 to-blue-200 p-8 rounded-lg shadow-lg max-w-md mx-auto space-y-6">
+            <img src={image} alt="First Image" className="w-full h-auto rounded-lg shadow-md" />
+            <img src={image1} alt="Second Image" className="w-full h-auto rounded-lg shadow-md" />
 
-<Link to="/side1" className='mt-9'> 
-
-<button className='bg-black text-white px-9 py-4 mt-9 '>
-Submite 
-</button>
-
-</Link>
-
+            <Link to="/side1" className='mt-6'>
+            <button className='bg-black text-white px-9 py-4 mt-9 '>
+            Submit
+                </button>
+            </Link>
         </div>
     );
 };
